46743f7f47c168123f2233b3379f62e4f3e62098
redluck <redluck@tiscali.it>
2026-01-27 10:04:56 +0100

    Creazione nel service del metodo di aggiunta

--- my_app/lib/firestore_service.dart
+++ my_app/lib/firestore_service.dart
@@ -6,4 +6,20 @@ class FirestoreService {
   Stream<QuerySnapshot> getPlaces() {
     return _firestore.collection('places').snapshots();
   }
+
+  Future<void> addPlace({
+    required String name,
+    required double latitude,
+    required double longitude,
+    String? description,
+    String? set,
+  }) {
+    return _firestore.collection('places').add({
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+      'description': description ?? '',
+      'set': set ?? '',
+    });
+  }
 }
